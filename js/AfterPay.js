0aaedcc596e317f63ffa2f911496a405215f6655
EroInnovations <eroinnovations9@gmail.com>
2025-04-01 08:59:57 +0300

    Main Site Pay Page

---
+++ js/AfterPay.js
@@ -0,0 +1,23 @@
+const START=()=>{
+
+    DISPLAY('',`
+
+        <header>
+
+            <h1 class='LeftText'>Elite Pay</h1>
+
+            <img src="${WHITEICONS}setting.png" class="RightIcon">
+        
+        </header>
+
+        <div class='MainDiv'>
+
+            <p>Thank You For Paying For Hosting</p>
+        
+        </div>
+
+    `);
+
+};
+
+START();
